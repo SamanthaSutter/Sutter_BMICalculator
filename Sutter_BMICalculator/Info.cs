1f31940c4ca132031cdb7ed0041ac353f7d8fbad
Student <Student@IT-DESKTOP>
2023-02-09 10:25:58 -0600

    Add project files.

---
+++ Sutter_BMICalculator/Info.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Sutter_BMICalculator
+{ 
+    class Info
+    {
+        public void DisplayInfo(string assignment)
+        {
+            const string BARRIER = "***************************************************";
+            WriteLine(BARRIER);
+            WriteLine("Name:\t\tSamantha Sutter");
+            WriteLine("Instructor:\tJanese Christie");
+            WriteLine("Assignment:\t" + assignment);
+            WriteLine("Date:\t\t" + DateTime.Today.ToShortDateString());
+            WriteLine(BARRIER);
+        }
+    }
+}
+
